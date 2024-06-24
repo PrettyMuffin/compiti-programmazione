c54649e598d4e311d62f6f565348f22c18700b16
PrettyMuffin <markobrunello2010@gmail.com>
2024-06-24 22:15:51 +0200

    rimozione elementi dalla lista

--- lista/lista.c
+++ lista/lista.c
@@ -76,16 +76,68 @@ void pop(Lista **lista)
     *lista = NULL;
 }
 
+void rimuovi_el(Lista **lista, int val)
+{
+    if (*lista == NULL)
+        return;
+    // Nel caso mi trovi al primo elemento faccio un'eliminazione alla testa della lista
+    if ((*lista)->value == val)
+    {
+
+        Lista *prossimo_el = (*lista)->nextNode;
+        free(*lista);
+        *lista = prossimo_el;
+        return;
+    }
+    else if ((*lista)->nextNode != NULL && (*lista)->nextNode->value == val)
+    {
+        // nel caso mi trovo all'ultimo elemento
+        if ((*lista)->nextNode->nextNode == NULL)
+        {
+            free((*lista)->nextNode);
+            (*lista)->nextNode = NULL;
+        }
+        else
+        {
+            // nel caso mi trovo in qualunque parte della lista, mi salvo il nodo successivo a quello che voglio
+            // eliminare
+            Lista *prossimo_el = (*lista)->nextNode->nextNode;
+            // libero la memoria del nodo che voglio elminare
+            free((*lista)->nextNode);
+            // elimino effettivamente il nodo sovvrascrivendo il campo nextNode del nodo precedente a quello eliminato con il nodo
+            //  successivo a quello elimnato (che mi sono salvato in prossmio_el)
+            (*lista)->nextNode = prossimo_el;
+        }
+        return;
+    }
+    rimuovi_el(&((*lista)->nextNode), val);
+}
+
+void rimozione_el_index(Lista **lista, int pos)
+{
+    if (pos == 0)
+    {
+        Lista *tmp = (*lista)->nextNode;
+        free(*lista);
+        *lista = tmp;
+        return;
+    }
+    rimozione_el_index(&((*lista)->nextNode), pos - 1);
+}
+
 int main()
 {
     Lista *mia_lista;
     init_list(&mia_lista);
     // pre_insert(&mia_lista, 34);
     suff_insert(&mia_lista, 14);
+    suff_insert(&mia_lista, 14);
     suff_insert(&mia_lista, 18);
     suff_insert(&mia_lista, 34);
-    pop(&mia_lista);
-    pop(&mia_lista);
-    pop(&mia_lista);
+    // pop(&mia_lista);
+    // pop(&mia_lista);
+    // pop(&mia_lista);
+    // rimuovi_el(&mia_lista, 34);
+    rimozione_el_index(&mia_lista, 3);
     stampa_lista(mia_lista);
 }
